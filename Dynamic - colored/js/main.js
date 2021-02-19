472e2b86cd9111e764eec8beffda6d871dc55d3d
MisaBackDev <misaelriot@hotmail.com>
2021-02-18 23:32:19 -0500

    [add] added transition in nav cta

--- Dynamic - colored/js/main.js
+++ Dynamic - colored/js/main.js
@@ -23,11 +23,13 @@ window.addEventListener("scroll", function () {
     document.querySelector("#navbar").style.opacity = 0.9;
     if (window.scrollY > 860){
         document.querySelector("#navbar").style.background = 'transparent';
+        document.querySelector('#navbar .nav-cta').style.background = 'black';
         document.querySelectorAll("#navbar>ul>li>a").forEach(item => item.style.color = '#040404');
 
     }else{
         document.querySelector("#navbar").style.opacity = 0.9;
         document.querySelector("#navbar").style.background = '#040404';
+        document.querySelector('#navbar .nav-cta').style.background = '#27ae60';
         document.querySelectorAll("#navbar>ul>li>a").forEach(item => item.style.color = 'white');
     }
 } else {
@@ -44,6 +46,7 @@ $("#navbar a").on("click", function (event) {
 
     const hash = this.hash;
     let offsetValue = hash == "#home" ? 125 : 0;
+
     $("html, body").animate(
       {
         scrollTop: $(hash).offset().top - offsetValue,
